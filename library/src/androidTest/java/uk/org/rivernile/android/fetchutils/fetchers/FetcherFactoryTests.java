f7b916e842dad0b12a2966bd10851b703a18f6d9
Niall Scott <riverniall@gmail.com>
2015-03-19 00:28:39 +0000

    Remove HttpFetcherTests as it was broken and provided no value. A more comprehensive unit test will be written for that later. Fixed broken tests in FetcherFactoryTests.

--- library/src/androidTest/java/uk/org/rivernile/android/fetchutils/fetchers/FetcherFactoryTests.java
+++ library/src/androidTest/java/uk/org/rivernile/android/fetchutils/fetchers/FetcherFactoryTests.java
@@ -28,108 +28,108 @@ public class FetcherFactoryTests extends InstrumentationTestCase {
     
     /**
      * Test that
-     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri, boolean)}
+     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri)}
      * returns {@code null} when the {@link Uri} is set to {@code null}.
      */
     public void testGetFetcherWithNullUri() {
-        assertNull(FetcherFactory.getFetcher(getInstrumentation().getContext(), null, true));
+        assertNull(FetcherFactory.getFetcher(getInstrumentation().getContext(), null));
     }
     
     /**
      * Test that
-     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri, boolean)}
+     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri)}
      * returns an instance of {@link HttpFetcher} when the {@link Uri} scheme is "http".
      */
     public void testGetFetcherWithHttp() {
         final Uri uri = Uri.parse("http://www.google.com/");
         final Fetcher fetcher = FetcherFactory.getFetcher(getInstrumentation()
-                .getContext(), uri, true);
+                .getContext(), uri);
         assertSame(HttpFetcher.class, fetcher.getClass());
     }
     
     /**
      * Test that
-     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri, boolean)}
+     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri)}
      * returns an instance of {@link HttpFetcher} when the {@link Uri} scheme is "https".
      */
     public void testGetFetcherWithHttps() {
         final Uri uri = Uri.parse("https://www.google.com/");
         final Fetcher fetcher = FetcherFactory.getFetcher(getInstrumentation()
-                .getContext(), uri, true);
+                .getContext(), uri);
         assertSame(HttpFetcher.class, fetcher.getClass());
     }
     
     /**
      * Test that
-     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri, boolean)}
+     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri)}
      * returns {@code null} when a path isn't given for the "android.asset" scheme.
      */
     public void testGetFetcherWithAndroidAssetAndNoPath() {
         final Uri uri = Uri.parse("android.asset://");
         final Fetcher fetcher = FetcherFactory.getFetcher(getInstrumentation()
-                .getContext(), uri, true);
+                .getContext(), uri);
         assertNull(fetcher);
     }
     
     /**
      * Test that
-     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri, boolean)}
+     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri)}
      * returns an instance of {@link AssetFileFetcher} when the {@link Uri} scheme is
      * "android.asset".
      */
     public void testGetFetcherWithAndroidAsset() {
         final Uri uri = Uri.parse("android.asset://test/image.jpg");
         final Fetcher fetcher = FetcherFactory.getFetcher(getInstrumentation()
-                .getContext(), uri, true);
+                .getContext(), uri);
         assertSame(AssetFileFetcher.class, fetcher.getClass());
     }
     
     /**
      * Test that
-     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri, boolean)}
+     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri)}
      * returns {@code null} when a path isn't given for the "file" scheme.
      */
     public void testGetFetcherWithFileAndNoPath() {
         final Uri uri = Uri.parse("file://");
         final Fetcher fetcher = FetcherFactory.getFetcher(getInstrumentation()
-                .getContext(), uri, true);
+                .getContext(), uri);
         assertNull(fetcher);
     }
     
     /**
      * Test that
-     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri, boolean)}
+     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri)}
      * returns an instance of {@link FileFetcher} when the {@link Uri} scheme is "file".
      */
     public void testGetFetcherWithFile() {
         final Uri uri = Uri.parse("file:///usr/tmp/something.txt");
         final Fetcher fetcher = FetcherFactory.getFetcher(getInstrumentation()
-                .getContext(), uri, true);
+                .getContext(), uri);
         assertSame(FileFetcher.class, fetcher.getClass());
     }
     
     /**
      * Test that
-     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri, boolean)}
+     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri)}
      * returns {@code null} when the {@link Uri} scheme is not handled.
      */
     public void testGetFetcherWithInvalidScheme() {
         final Uri uri = Uri.parse("invalid://test/image.jpg");
         final Fetcher fetcher = FetcherFactory
-                .getFetcher(getInstrumentation().getContext(), uri, true);
+                .getFetcher(getInstrumentation().getContext(), uri);
         assertNull(fetcher);
     }
     
     /**
      * Test that
-     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri, boolean)}
+     * {@link FetcherFactory#getFetcher(android.content.Context, android.net.Uri)}
      * returns {@code null} when the {@link Uri} scheme is not handled and the {@link Uri} is not
      * hierarchical.
      */
     public void testGetFetcherWithInvalidScheme2() {
         final Uri uri = Uri.parse("mailto:someone@mail.com");
         final Fetcher fetcher = FetcherFactory
-                .getFetcher(getInstrumentation().getContext(), uri, true);
+                .getFetcher(getInstrumentation().getContext(), uri);
         assertNull(fetcher);
     }
 }
